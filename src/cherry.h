1f99e21c23f32fcc4bd2185af6cf1710c98f413a
aindros <aindros@hotmail.com>
2020-05-13 01:45:16 +0200

    Sources, samples and makefile (first alpha version)

---
+++ src/cherry.h
@@ -0,0 +1,44 @@
+/*-
+ * Copyright (c) 2020 Alessandro Iezzi
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#ifndef __CHERRY_H__
+#define __CHERRY_H__
+
+#include <stdio.h>
+
+#ifdef _WIN64
+#include <Windows.h>
+#define CHERRY_LIB __declspec(dllexport)
+#endif /* WIN32 */
+
+
+typedef struct CherryApplication {
+  char *name;
+} CherryApplication;
+
+CHERRY_LIB CherryApplication *cherry_application_new(const char *);
+CHERRY_LIB int                cherry_application_run();
+
+#endif /* __CHERRY_H__ */
