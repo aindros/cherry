8ba33266ae85962a26f372f47dcea0572cd1c7c1
Alessandro Iezzi <aiezzi@alessandroiezzi.it>
2022-12-21 15:19:15 +0100

    Update copyright

--- src/cherry.c
+++ src/cherry.c
@@ -1,5 +1,5 @@
 /*-
- * Copyright (c) 2020 Alessandro Iezzi
+ * Copyright (c) 2020-2022  Alessandro Iezzi <aiezzi AT alessandroiezzi PERIOD it>
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
