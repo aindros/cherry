5b77d7f309deaf4fc59377094adf02ba9ed15402
Alessandro Iezzi <aiezzi@alessandroiezzi.it>
2023-05-17 17:25:21 +0200

    Add color.h

---
+++ src/color.h
@@ -0,0 +1,8 @@
+/* See LICENSE file for copyright and license details. */
+
+#ifndef __CHERRY_COLOR_H__
+#define __CHERRY_COLOR_H__
+
+#define RGB(r, g, b) (b + (g<<8) + (r<<16))
+
+#endif /* __CHERRY_COLOR_H__ */
