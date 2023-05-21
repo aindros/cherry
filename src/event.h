3312c849325d171984c9d32a0f093cd6a2e330c6
Alessandro Iezzi <aiezzi@alessandroiezzi.it>
2023-05-21 23:30:44 +0200

    Define a structure for a CherryEvent

--- src/event.h
+++ src/event.h
@@ -3,12 +3,21 @@
 #ifndef __CHERRY_EVENT_H__
 #define __CHERRY_EVENT_H__
 
+#include <X11/Xlib.h>
 
 enum Events {
 	WINDOW_DELETED,
 	WINDOW_EXPOSED,
 	MOUSE_BUTTON_PRESSED,
 	KEY_PRESSED
+};
+
+typedef struct CherryEvent {
+	Display *display;
+	Window   window;
+	int      event_id;
+	int      x, y;
+	XKeyEvent xkey;
 };
 
 #endif /* __CHERRY_EVENT_H__ */
