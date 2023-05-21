771264099cf72d75d479eb21991b01988d75dcfc
Alessandro Iezzi <aiezzi@alessandroiezzi.it>
2023-05-21 23:29:31 +0200

    Define an enum to identify an event type

--- src/event.h
+++ src/event.h
@@ -3,6 +3,12 @@
 #ifndef __CHERRY_EVENT_H__
 #define __CHERRY_EVENT_H__
 
-#define DELETE_WINDOW 0
+
+enum Events {
+	WINDOW_DELETED,
+	WINDOW_EXPOSED,
+	MOUSE_BUTTON_PRESSED,
+	KEY_PRESSED
+};
 
 #endif /* __CHERRY_EVENT_H__ */
